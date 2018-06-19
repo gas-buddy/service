5b5d742fa530ced74f40776a5a78e6f1e7d8132d
Matthew Pavlinsky <mpavlinsky@gmail.com>
2018-06-19 10:02:15 -0400

    Add options parameter and domain override to throwError (#50)

--- src/util.js
+++ src/util.js
@@ -50,15 +50,26 @@ export function winstonError(error) {
 /**
  * Build and throw a well formed error to be caught and sent in finalHandler
  */
-export function throwError(serviceName, codeOrError = 'nocode', message = 'nomessage', status = 500, domain) {
+export function throwError(serviceName, codeOrError = 'nocode', message = 'nomessage', status = 500, domainOrOpts, opts = {}) {
   let error = codeOrError;
   if (!(error instanceof Error)) {
     error = new Error(message);
   }
 
+  const isOpts = domainOrOpts instanceof Object;
+  const finalOpts = isOpts ? domainOrOpts : opts;
+  const domain = (!isOpts && domainOrOpts) || serviceName;
+
   error.code = error.code || codeOrError;
   error.status = error.status || status;
-  error.domain = error.domain || domain || serviceName;
+  error.domain = error.domain || domain;
+
+  if (finalOpts) {
+    const displayMessage = finalOpts.displayMessage;
+    if (displayMessage) {
+      error.displayMessage = (displayMessage === !!displayMessage) ? error.message : displayMessage;
+    }
+  }
 
   throw error;
 }
