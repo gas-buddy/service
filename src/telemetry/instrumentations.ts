b909f66a94c02b1b1871db379aa98a6ec1f87692
Max Metral <max@pyralis.com>
2022-10-15 20:52:30 -0400

    Remove some unused dependencies

--- src/telemetry/instrumentations.ts
+++ src/telemetry/instrumentations.ts
@@ -5,15 +5,11 @@ import { AwsInstrumentation } from '@opentelemetry/instrumentation-aws-sdk';
 import { DnsInstrumentation } from '@opentelemetry/instrumentation-dns';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { GenericPoolInstrumentation } from '@opentelemetry/instrumentation-generic-pool';
-import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
-import { LruMemoizerInstrumentation } from '@opentelemetry/instrumentation-lru-memoizer';
 import { NetInstrumentation } from '@opentelemetry/instrumentation-net';
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
 import { PinoInstrumentation } from '@opentelemetry/instrumentation-pino';
-import { RedisInstrumentation as RedisInstrumentationV2 } from '@opentelemetry/instrumentation-redis';
-import { RedisInstrumentation as RedisInstrumentationV4 } from '@opentelemetry/instrumentation-redis-4';
 
 const InstrumentationMap = {
   // Disable this for now because it logs a stupid message
@@ -22,15 +18,11 @@ const InstrumentationMap = {
   '@opentelemetry/instrumentation-dns': DnsInstrumentation,
   '@opentelemetry/instrumentation-express': ExpressInstrumentation,
   '@opentelemetry/instrumentation-generic-pool': GenericPoolInstrumentation,
-  '@opentelemetry/instrumentation-graphql': GraphQLInstrumentation,
   '@opentelemetry/instrumentation-http': HttpInstrumentation,
   '@opentelemetry/instrumentation-ioredis': IORedisInstrumentation,
-  '@opentelemetry/instrumentation-lru-memoizer': LruMemoizerInstrumentation,
   '@opentelemetry/instrumentation-net': NetInstrumentation,
   '@opentelemetry/instrumentation-pg': PgInstrumentation,
   '@opentelemetry/instrumentation-pino': PinoInstrumentation,
-  '@opentelemetry/instrumentation-redis': RedisInstrumentationV2,
-  '@opentelemetry/instrumentation-redis-4': RedisInstrumentationV4,
 };
 
 // Config types inferred automatically from the first argument of the constructor
