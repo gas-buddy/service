a8e27c83b76b3c4e4986eaeeb21311e145a22a99
Dhruv Patel <dhruv.patel@pditechnologies.com>
2024-10-11 10:55:42 -0600

    chore: Remove console statement

--- __tests__/fake-job.test.ts
+++ __tests__/fake-job.test.ts
@@ -30,7 +30,6 @@ describe('fake-job', () => {
 
       // Verify that the runId provided to service run does get applied in locals
       expect(app.locals.runId).toEqual('generated-uuid-123456789');
-      console.log('loggerSpy.mock.lastCall: ', loggerSpy.mock.lastCall);
       expect(loggerSpy.mock.lastCall).toHaveBeenCalledWith({
         trace_id: 'generated-uuid-123456789',
       });
